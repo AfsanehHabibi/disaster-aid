c44e935b96bea07919dbdbf1d19ce96d38914114
akramsadat safavimoghadam <safavimoghadam.78@gmail.com>
2020-07-06 23:39:18 +0430

    create login component

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -1,84 +1,101 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import { Form, Input, Button, Checkbox } from 'antd';
+import React, { useState } from "react";
+import { NavLink as RouterNavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const layout = {
-  labelCol: {
-    span: 5,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-const tailLayout = {
-   
-  wrapperCol: {
-    offset: 5,
-    span: 16,
-  },
-};
 
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Login = () => {
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
+  const [isOpen, setIsOpen] = useState(false);
+  const {
+    user,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+  const toggle = () => setIsOpen(!isOpen);
 
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: "http://localhost:3000/login",
+    });
 
   return (
-     <div id="id">
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-         
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <div className="nav-container">
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand className="logo" />
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="d-none d-md-block" navbar>
+             
+              {isAuthenticated && (
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret id="profileDropDown">
+                    <img
+                      src={user.picture}
+                      alt="Profile"
+                      className="nav-user-profile rounded-circle"
+                      width="50"
+                    />
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem header>{user.name}</DropdownItem>
+                    <DropdownItem
+                      tag={RouterNavLink}
+                      to="/profile"
+                      className="dropdown-profile"
+                      activeClassName="router-link-exact-active"
+                    >
+                      <FontAwesomeIcon icon="user" className="mr-3" /> Profile
+                    </DropdownItem>
+                    <DropdownItem
+                      id="qsLogoutBtn"
+                      onClick={() => logoutWithRedirect()}
+                    >
+                      <FontAwesomeIcon icon="power-off" className="mr-3" /> 
+                      Log out
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              )}
+            </Nav>
+            {!isAuthenticated && (
+              <Nav className="d-md-none" navbar>
+                <NavItem>
+                  <Button
+                    id="qsLoginBtn"
+                    color="primary"
+                    block
+                    onClick={() => loginWithRedirect({})}
+                  >
+                    Log in
+                  </Button>
+                </NavItem>
+              </Nav>
+            )}
+           
+          </Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
 
 export default Login;
-
