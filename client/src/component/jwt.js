623f2d8748a06091aa517ffe0cbdbaa81330f56e
akramsadat safavimoghadam <safavimoghadam.78@gmail.com>
2020-07-16 22:55:56 +0430

    add headers for token

--- client/src/component/jwt.js
+++ client/src/component/jwt.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { Button, Alert } from "reactstrap";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-
-export const JwtComponent = () => {
+export const ExternalApiComponent = () => {
   const [state, setState] = useState({
     showResult: false,
     apiMessage: "",
@@ -12,14 +11,55 @@ export const JwtComponent = () => {
 
   const {
     getAccessTokenSilently,
+    loginWithPopup,
+    getAccessTokenWithPopup,
   } = useAuth0();
 
+  const handleConsent = async () => {
+    try {
+      await getAccessTokenWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const handleLoginAgain = async () => {
+    try {
+      await loginWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
 
   const callApi = async () => {
     try {
       const token = await getAccessTokenSilently();
+      const baseUrl = 'http://localhost:5000';
+      const response = await fetch(`${baseUrl}/api/private`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-      const responseData = await token;
+      const responseData = await response.json();
 
       setState({
         ...state,
@@ -34,17 +74,64 @@ export const JwtComponent = () => {
     }
   };
 
+  const handle = (e, fn) => {
+    e.preventDefault();
+    fn();
+  };
 
   return (
     <>
       <div className="mb-5">
+        {state.error === "consent_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              class="alert-link"
+              onClick={(e) => handle(e, handleConsent)}
+            >
+              consent to get access to users api
+            </a>
+          </Alert>
+        )}
+
+        {state.error === "login_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              class="alert-link"
+              onClick={(e) => handle(e, handleLoginAgain)}
+            >
+              log in again
+            </a>
+          </Alert>
+        )}
+
+        <h1>External API</h1>
+        <p>
+          Ping an external API by clicking the button below. This will call the
+          external API using an access token, and the API will validate it using
+          the API's audience value.
+        </p>
+
         <Button color="primary" className="mt-5" onClick={callApi}>
           Ping API
         </Button>
       </div>
-      {state.apiMessage}
+
+      <div className="result-block-container">
+        {state.showResult && (
+          <div className="result-block" data-testid="api-result">
+            <h6 className="muted">Result</h6>
+    
+              <span>{JSON.stringify(state.apiMessage, null, 2)}</span>
+          
+          </div>
+        )}
+      </div>
     </>
   );
 };
 
-export default withAuthenticationRequired(JwtComponent);
+export default withAuthenticationRequired(ExternalApiComponent);
