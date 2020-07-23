e6867fb86df7754739d0735eb074e4e16a2b1810
akramsadat safavimoghadam <safavimoghadam.78@gmail.com>
2020-07-23 18:14:52 +0430

    create button

---
+++ client/src/component/detail.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { NavLink as RouterNavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+import {
+    Collapse,
+    Container,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Button,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+  } from "reactstrap";
+
+export const Detail = () => {
+
+  const {
+    user,
+    isAuthenticated,
+    getAccessToken,
+    loginWithRedirect,
+    getAccessTokenSilently,
+    logout,
+  } = useAuth0();
+
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: `${process.env.REACT_APP_PUBLIC_URL}`,
+    });
+
+  var userJson=Object.values(user)
+  var firstvalue=""
+  var secondvalue=""
+  var firstpath=""
+  var secondpath=""
+
+  for(var i=0; i<4 ; i++){
+        if(userJson[i]!="noting"){
+            firstvalue=userJson[i]
+        }
+  }
+ 
+  if(firstvalue==="Field Agents"){
+    firstvalue = "report";
+    firstpath = "/forms";
+   }else if(firstvalue ==="Control Centre Agent"){
+    firstvalue = "summery";
+    firstpath ="/controlCentre";
+   }else if(firstvalue ==="Admin"){
+    firstvalue = "report";
+    firstpath = "/forms";
+    secondvalue = "summery";
+    secondpath ="/controlCentre";
+   }
+
+  const menu = (
+    <Menu>
+
+      <Menu.Item>
+        <Button
+        tag={RouterNavLink}
+        to={firstpath}
+        activeClassName="router-link-exact-active"
+        >
+          <FontAwesomeIcon icon="user" className="mr-3" /> {firstvalue}
+        </Button>
+      </Menu.Item>
+
+      <Menu.Item>
+        <Button
+        tag={RouterNavLink}
+        to={secondpath}
+        activeClassName="router-link-exact-active"
+        >
+          {secondvalue}
+        </Button>
+      </Menu.Item>
+
+      <Menu.Item>
+        <a  onClick={() => logoutWithRedirect()} >
+          <FontAwesomeIcon icon="power-off" className="mr-3" /> Log out
+        </a>
+      </Menu.Item>
+
+    </Menu>
+
+   );
+
+   return(
+    <div>
+    <Dropdown overlay={menu}>
+       <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+        detail <DownOutlined />
+       </a>
+     </Dropdown>
+     </div>
+   )
+};
+
+export default withAuthenticationRequired(Detail);
+
