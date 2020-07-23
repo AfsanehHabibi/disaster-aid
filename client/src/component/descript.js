7ceffdb874c9435e92771827594c5a3df573cbab
akramsadat safavimoghadam <safavimoghadam.78@gmail.com>
2020-07-23 22:55:23 +0430

    write welcome in entering page

--- client/src/component/descript.js
+++ client/src/component/descript.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Typography } from 'antd';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Descript = () => {
   
+  const { Title } = Typography;
+  const { user, isAuthenticated } = useAuth0();
+
   return (
-    <div >
-        <p>hellllllooooooooooooooooooooooo</p>
-    </div>
+    isAuthenticated && (
+        <div>
+          <Title style={{ textAlign: "center"}} >welcome {user.nickname.split(".78")}</Title>
+        </div>
+    )
   );
+
 };
 
 export default Descript;
