a953d7d94d742bc034e344a8d28deacfeacaea22
fateme <fatemezafarpour78@gmail.com>
2020-07-24 20:48:18 +0430

    add filter to area fields

--- client/src/component/summaryTable.js
+++ client/src/component/summaryTable.js
@@ -61,15 +61,16 @@ export class SummaryTable extends React.Component {
     let columnType = [];
     let hasAnyNumber = false;
 
-    let index = 0;
+    let index = 0 , areaIndex = 0;
     const columns = [];
     formDescriptor.fields.map((field, i) => {
       columnType[index] = field.type;
       columns.push({title :  field.title , dataIndex : field.name })
       if(field.type === "Location"){
-        columns.push({title : field.title+" area", dataIndex : field.name+"_area"})
+        columns.push({title : field.title+" area", dataIndex : "_area_"+areaIndex})
         index++;
-        columnType[index] = "area";
+        areaIndex++;
+        columnType[index] = "Area";
       }
       if(field.type === "Number"){
         hasAnyNumber = true;
@@ -79,6 +80,8 @@ export class SummaryTable extends React.Component {
     const num_of_columns = index;
     console.log(columns);
 
+    let areasName = [];
+    areaIndex = 0;
     const rowdata = filledForms.map((filledForm , index)=>{
       let f = filledForm.fields;
       let t = {};
@@ -105,17 +108,21 @@ export class SummaryTable extends React.Component {
       }
       if(f.location_fields!==undefined  &&  f.location_fields !== null){
         f.location_fields.map((loc_field , i)=>{
+          let names = [];
           let key = loc_field.name;
           let val = loc_field.value.coordinates[0] + " , " +loc_field.value.coordinates[1] ;
         
-          let key2 = loc_field.name+"_area";
+          let key2 ="_area_" + areaIndex;
           let val2 = "";
           loc_field.areasDoc.map((area , i)=>{
             val2 += area.properties.name + " ";
+            names[i] = area.properties.name;
           })
   
           t[key] = val;
           t[key2] = val2;
+          areasName.push(names)
+          areaIndex++;
         })
       }
 
@@ -123,8 +130,10 @@ export class SummaryTable extends React.Component {
     
     })
 
+    console.log('areaNames ',areasName)
     //calculate sum for number fields
     let sum = [];
+    areaIndex = 0;
     for(let col = 0; col < num_of_columns; col++){
       if(columnType[col] === "Number"){
         sum[col] = 0;
@@ -134,6 +143,15 @@ export class SummaryTable extends React.Component {
         })
       }else{
         sum[col] = "";
+      }
+      if(columnType[col] === "Area"){
+        //add filters and onFilter to column
+        columns[col].filters = areasName[areaIndex].map((n , i)=>{
+          return({text : n , value : n})
+        })
+        let index = "_area_" + areaIndex;
+        columns[col].onFilter = (value, record) => record[index].indexOf(value) !== -1
+        areaIndex++;
       }
     }
     
