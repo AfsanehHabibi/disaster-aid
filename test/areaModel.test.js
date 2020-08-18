202dfc826973b989213ab62ce79457d3d3c57a16
Afsaneh <a.habibi1394@gmail.com>
2020-08-18 10:05:38 +0430

    Add test for area model

---
+++ test/areaModel.test.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose')
+const { startDatabase } = require('../src/database/mongo');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
+const { beforeEach } = require('mocha');
+const continents = require('./databse/continent/continentsWithName.json');
+const { assert } = require('chai');
+const areaModel = require('../src/models/area').areaModel
+
+describe('area model ', () => {
+    before((done) => {
+        startDatabase(() => {
+            //this function runs after the drop is completed
+            done(); //go ahead everything is done now.
+        });
+    })
+    beforeEach((done) => {
+        mongoose.connection.collections.areas.drop(() => {
+            //this function runs after the drop is completed
+            done(); //go ahead everything is done now.
+        });
+    })
+    it('create many area', async function () {
+        let areas = await areaModel.insertMany(continents.features);
+        areas.should.have.length(8);
+    })
+    after((done) => {
+        mongoose.disconnect(() => {
+            //this function runs after the drop is completed
+            done(); //go ahead everything is done now.
+        });
+    })
+}
+);
